e3190f1c8d907b7bd418b3d47e3deca3422de423
Pyeongseok Oh <cseteram.dev@gmail.com>
2022-10-14 19:36:12 +0900

    Add value file for ingress-nginx

--- helm.tf
+++ helm.tf
@@ -6,28 +6,7 @@ resource "helm_release" "ingress_nginx" {
   chart      = "ingress-nginx"
   version    = "4.3.0"
 
-  set {
-    name  = "controller.service.type"
-    value = "NodePort"
-  }
-
-  set {
-    name  = "controller.service.nodePorts.http"
-    value = "30080"
-  }
-
-  set {
-    name  = "controller.service.nodePorts.https"
-    value = "30443"
-  }
-
-  set {
-    name  = "controller.publishService.enabled"
-    value = "false"
-  }
-
-  set {
-    name  = "controller.extraArgs.publish-status-address"
-    value = "sekai.cseteram.dev"
-  }
+  values = [
+    file("helm/ingress-nginx.yaml")
+  ]
 }
