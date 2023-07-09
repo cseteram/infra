442da9190296923a93d93e0a25ba4905103fb6c3
Pyeongseok Oh <cseteram.dev@gmail.com>
2023-07-10 01:00:57 +0900

    Disable haproxy and adjust controller role

--- sekai.tf
+++ sekai.tf
@@ -39,43 +39,43 @@ resource "aws_lightsail_instance" "sekaiv2_controller" {
     curl -sSL https://github.com/${local.owner}.keys >> /home/ubuntu/.ssh/authorized_keys
 
     # Install haproxy
-    sudo apt update
-    sudo apt install -y haproxy
-    cat > /etc/haproxy/haproxy.cfg << EOF
-    global
-        log /dev/log local0
-        log /dev/log local1 notice
-        chroot /var/lib/haproxy
-        stats socket /run/haproxy/admin.sock mode 660 level admin expose-fd listeners
-        stats timeout 30s
-        user haproxy
-        group haproxy
-        daemon
+    # sudo apt update
+    # sudo apt install -y haproxy
+    # cat > /etc/haproxy/haproxy.cfg << EOF
+    # global
+    #     log /dev/log local0
+    #     log /dev/log local1 notice
+    #     chroot /var/lib/haproxy
+    #     stats socket /run/haproxy/admin.sock mode 660 level admin expose-fd listeners
+    #     stats timeout 30s
+    #     user haproxy
+    #     group haproxy
+    #     daemon
 
-    defaults
-        log     global
-        mode    tcp
-        option  httplog
-        option  dontlognull
-        timeout connect 5000
-        timeout client  50000
-        timeout server  50000
+    # defaults
+    #     log     global
+    #     mode    tcp
+    #     option  httplog
+    #     option  dontlognull
+    #     timeout connect 5000
+    #     timeout client  50000
+    #     timeout server  50000
 
-    listen k0s-http
-        bind *:80
-        server k0s 127.0.0.1:30080
+    # listen k0s-http
+    #     bind *:80
+    #     server k0s 127.0.0.1:30080
 
-    listen k0s-https
-        bind *:443
-        server k0s 127.0.0.1:30443
-    EOF
-    sudo systemctl reload haproxy
+    # listen k0s-https
+    #     bind *:443
+    #     server k0s 127.0.0.1:30443
+    # EOF
+    # sudo systemctl reload haproxy
   EOT
 
   tags = {
     Clutser   = local.cluster_name
     Name      = local.controller.name
-    Role      = "controller+worker"
+    Role      = "controller"
     Terraform = "true"
   }
 }
